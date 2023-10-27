bf87a391f50f3cb9921a22e6a3bdddaaa06418c3
yangyao <y.yang@aftership.com>
2023-10-27 17:20:41 +0800

    Remobe variant/update webhook

--- Constants.php
+++ Constants.php
@@ -36,7 +36,6 @@ class Constants
     public const WEBHOOK_TOPIC_ORDERS_UPDATE = 'orders/update';
     public const WEBHOOK_TOPIC_ORDERS_DELETE = 'orders/delete';
     public const WEBHOOK_TOPIC_PRODUCTS_UPDATE = 'products/update';
-    public const WEBHOOK_TOPIC_VARIANTS_UPDATE = 'variants/update';
     public const WEBHOOK_TOPIC_PRODUCTS_DELETE = 'products/delete';
     public const WEBHOOK_TOPIC_VARIANTS_DELETE = 'variants/delete';
 }
