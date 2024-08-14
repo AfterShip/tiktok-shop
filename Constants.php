5970f5fe85f51de973a3cfac3547e4f48821f671
Gali13 <985831128@qq.com>
2024-08-14 17:52:15 +0800

    modify magento version

--- Constants.php
+++ Constants.php
@@ -33,7 +33,7 @@ class Constants
 
     public const WEBHOOK_RESOURCE_CREDITMEMOS = 'creditmemos';
 
-    public const AFTERSHIP_TIKTOK_SHOP_VERSION = '1.0.17';
+    public const AFTERSHIP_TIKTOK_SHOP_VERSION = '1.0.18';
 
     public const WEBHOOK_CONFIG_SCOPE_PATH = 'aftership/webhooks/webhooks';
 
