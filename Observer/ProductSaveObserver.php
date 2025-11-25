3ea74e2d2c65b44fa995672ba1f8bbe26392e638
Anna <xt.huang@aftership.com>
2025-11-25 15:17:29 +0800

    optimize product webhook

--- Observer/ProductSaveObserver.php
+++ Observer/ProductSaveObserver.php
@@ -17,6 +17,9 @@ use Magento\Framework\Event\ObserverInterface;
 use AfterShip\TikTokShop\Model\Queue\WebhookPublisher;
 use Psr\Log\LoggerInterface;
 use AfterShip\TikTokShop\Model\Api\WebhookEvent;
+use Magento\Framework\Api\ExtensionAttributesFactory;
+use AfterShip\TikTokShop\Api\Data\ProductChangeType;
+use Magento\Framework\ObjectManagerInterface;
 
 /**
  * Send webhook when get product update event.
@@ -47,20 +50,40 @@ class ProductSaveObserver implements ObserverInterface
     protected $commonHelper;
 
     /**
+     * Extension attributes factory
+     *
+     * @var ExtensionAttributesFactory
+     */
+    protected $extensionAttributesFactory;
+
+    /**
+     * Object manager
+     *
+     * @var ObjectManagerInterface
+     */
+    protected $objectManager;
+
+    /**
      * Construct
      *
      * @param LoggerInterface $logger
      * @param WebhookPublisher $publisher
      * @param CommonHelper $commonHelper
+     * @param ExtensionAttributesFactory $extensionAttributesFactory
+     * @param ObjectManagerInterface $objectManager
      */
     public function __construct(
         LoggerInterface  $logger,
         WebhookPublisher $publisher,
-        CommonHelper $commonHelper
+        CommonHelper $commonHelper,
+        ExtensionAttributesFactory $extensionAttributesFactory,
+        ObjectManagerInterface $objectManager
     ) {
         $this->logger = $logger;
         $this->publisher = $publisher;
         $this->commonHelper = $commonHelper;
+        $this->extensionAttributesFactory = $extensionAttributesFactory;
+        $this->objectManager = $objectManager;
     }
 
     /**
@@ -82,10 +105,25 @@ class ProductSaveObserver implements ObserverInterface
             /* @var \Magento\Catalog\Model\Product $product */
             $product = $observer->getEvent()->getProduct();
             $productId = $product->getId();
-            $event = new WebhookEvent();
+            $event = $this->objectManager->create(WebhookEvent::class);
             $event->setId($productId)
                 ->setResource(Constants::WEBHOOK_RESOURCE_PRODUCTS)
                 ->setEVent(Constants::WEBHOOK_EVENT_UPDATE);
+            
+            $extensionAttributes = null;
+            if (method_exists($event, 'getExtensionAttributes')) {
+                $extensionAttributes = $event->getExtensionAttributes();
+            }
+            if (!$extensionAttributes) {
+                $extensionAttributes = $this->extensionAttributesFactory->create(
+                    WebhookEvent::class
+                );
+            }
+            $extensionAttributes->setProductChangeType(ProductChangeType::BASIC);
+            if (method_exists($event, 'setExtensionAttributes')) {
+                $event->setExtensionAttributes($extensionAttributes);
+            }
+            
             $this->publisher->execute($event);
         } catch (\Exception $e) {
             $this->logger->error(
